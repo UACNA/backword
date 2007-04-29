d104209a920da0656c6d389ba48342c4b22cff02
Gneheix <Gneheix@22fa8185-5620-0410-bde6-f91db22e34cc>
2007-04-29 17:56:02 +0000

    delete functions on reveiw page

--- src/chrome/content/review.js
+++ src/chrome/content/review.js
@@ -1,54 +1,164 @@
-var backword = new BW_Layout();
-backword._api = new BW_LocalAPI('backword/');
+var backword = new BW_Layout(false);
+var api = new BW_LocalAPI('backword/');
+api.observe = function(aSubject, aTopic, aData){
+	if (aTopic == "bw_load_storage" && aSubject != this){
+		location.reload();
+	}
+};
+var words = api._words;
+var backwordReviewPage = true;
+
+function BW_ReviewPage(dict){
+	this.dictionary = null;
+	this.currentTranslator = "";
+	this.backword = backword;
+}
+
+BW_ReviewPage.prototype.observe = function(aSubject, aTopic, aData){
+	if (aTopic == "bw_dictionary_changed"){
+		this.setDictionary(aData);
+	}
+};
+
+BW_ReviewPage.prototype.QueryInterface = function(aIID){
+  if (!aIID.equals(Components.interfaces.nsIObserver) &&
+      !aIID.equals(Components.interfaces.nsISupports))
+    throw Components.results.NS_ERROR_NO_INTERFACE;
+   return this;
+};
+
+BW_ReviewPage.prototype.setDictionary = function(translator){
+	if (this.currentTranslator != translator) {
+		this.currentTranslator = translator;
+		if (translator.substr(0, 6) == "google") {
+			this.dictionary = new BW_GoogleTranslate(translator.substr(7));
+		} else {
+			this.dictionary = new BW_DictcnTranslate(translator == "dictcn.tw");
+		}
+	}
+}
+
+var page = new BW_ReviewPage();
 
 function doLoad(){
-	var panel = document.getElementById('panel');
+	var panel = $('panel');
 	var html = "";
-	var words = backword._api._words;
+	buildMatchPattern();
 	for (var i=words.length-1; i>=0; i--){
 		html += formatWord(words[i]);
 	}
-	for (var i=words.length-1; i>=0; i--){
-		var re = new RegExp("(>[^<]*[^A-Za-z])"+words[i].re.replace(/\?/,"")+"(?!(<\\/span>|[A-Za-z]))", "gi");
-		html = html.replace(re, "$1<a href='#word-"+words[i].id+"' class='innerlink'>$2</a>");
-	}
 	panel.innerHTML = html;
+	attachEvent();
+	registerObserve();
 }
 
-function getReForWord(word){
-	var re;
-	if (/f$/.test(word.id)){
-		re = "("+word.id.replace(/f$/, "") + "[fv](s|es|ies|d|ed|ied|ing){0,2})";
-	}
-	else{
-		if (/[^aieouy]$/.test(word.id)) {
-			re = "("+word.id + "{1,2}(s|es|ies|d|ed|ied|ing){0,2})";
-		} else {
-			if (/[ey]$/.test(word.id)) {
-				re = "("+word.id + "?(s|es|ies|d|ed|ied|ing){0,2})";
+function registerObserve(){
+	var observerService = Components.
+	  classes["@mozilla.org/observer-service;1"].
+	  getService(Components.interfaces.nsIObserverService);
+	observerService.addObserver(page, "bw_dictionary_changed", false);	
+	Components.classes['@mozilla.org/observer-service;1'].getService(Components.interfaces.nsIObserverService).notifyObservers(window, 'bw_review_page_opened', '');
+}
+
+function buildMatchPattern(){
+	for (var i=words.length-1; i>=0; i--){
+		var word = words[i];
+		if (/f$/.test(word.id)){
+			re = "("+word.id.replace(/f$/, "") + "[fv](s|es|ies|d|ed|ied|ing){0,2})";
+		}
+		else{
+			if (/[^aieouy]$/.test(word.id)) {
+				re = "("+word.id + "{1,2}(s|es|ies|d|ed|ied|ing){0,2})";
 			} else {
-				re = "("+word.id + "(s|es|ies|d|ed|ied|ing){0,2})";
+				if (/[ey]$/.test(word.id)) {
+					re = "("+word.id + "?(s|es|ies|d|ed|ied|ing){0,2})";
+				} else {
+					re = "("+word.id + "(s|es|ies|d|ed|ied|ing){0,2})";
+				}
 			}
 		}
+		word.regHighLight = new RegExp(re, "gi");
+		word.regLink = new RegExp("([^a-z])"+re+"(?!<\\/span>|[a-z])", "gi");
 	}
-	word.re = re;
-	return re;
 }
 
 function formatWord(word){
-	var Reg = new RegExp(getReForWord(word), "gi");
-	var html = '<a name="word-'+word.id+'"/><div id="word-'+word.id+'" class="word"><span class="wordid">'+
-		word.id+'</span><span class="wordparaphrase">'+word.paraphrase+'<span>';
+	var html = '<a name="word-'+word.id+'"/><div id="word-'+word.id+'" class="word"><img src="chrome://backword/skin/backWordED.gif" class="delete" id="todelete-word-'+word.id
+	+'"/><img src="chrome://backword/skin/backWordOK.gif" class="delete" style="display:none;" id="delete-word-'+word.id+'"/><span class="wordid" id="'+word.id+'">'+
+		word.id+'</span><span class="wordparaphrase" id="paraphrase-'+word.id+'">'+word.paraphrase+'</span><span class="wordparaphrase" id="translation-'+word.id+'"></span>';
 	for (var i=0; i<word.quotes.length; i++){
-		html += formatQuote(word.quotes[i], Reg);
+		html += formatQuote(word.quotes[i], word);
 	}
 	html+='</div>';
 	return html;
 }
 
-function formatQuote(quote, Reg){
-	var html = '<div id="quote-'+quote.id+'" class="quote">&raquo;<a href="'+quote.url+'" class="quoteurl"><span class="quotetitle">'+
-		quote.title+'</span></a><div class="quoteparagraph">'+quote.paragraph.replace(Reg, "<span class='theword'>$1</span>")+'</div></div>';
+function formatQuote(quote, word){
+	var html = '<div id="quote-'+quote.id+'" class="quote"><img src="chrome://backword/skin/backWordED.gif" class="delete" id="todelete-quote-'+word.id+'-'+quote.id
+	+'"/><img src="chrome://backword/skin/backWordOK.gif" class="delete" style="display:none;" id="delete-quote-'+word.id+'-'+quote.id+'"/>&raquo;<a href="'+quote.url+'" class="quoteurl"><span class="quotetitle">'+
+		quote.title+'</span></a><div class="quoteparagraph">'+formatQuoteParagraph(quote,word)+'</div></div>';
 	return html;
 }
+
+function formatQuoteParagraph(quote, word){
+    var html = quote.paragraph.replace(word.regHighLight,  "<span class='theword'>$1</span>");
+	for (var i=words.length-1; i>=0; i--){
+		html = html.replace(words[i].regLink, "$1<a href='#word-"+words[i].id+"' class='innerlink'>$2</a>");
+	}
+	return html;
+}
+
+function attachEvent(){
+    attachClicktoParaphrase();
+    attachDeleteButton();
+}
+
+function attachClicktoParaphrase(){
+	for (var i=words.length-1; i>=0; i--){
+	    var el = $(words[i].id);
+	    el.onmouseover = function(){
+		    $('translation-'+this.id).innerHTML = "|" + page.dictionary.getTranslate(this.id);
+	    }
+	    el.onmouseout = function(){
+		    $('translation-'+this.id).innerHTML = "";
+	    }
+	}
+}
+
+function attachDeleteButton(){
+	imgs = document.getElementsByTagName("img");
+	for(var index=0; index<imgs.length; index++) {
+		var img = imgs[index];
+		if (img.id.indexOf('todelete') == 0){
+			img.onmouseover = function(){
+				this.src="chrome://backword/skin/backWordE.gif";
+			}
+			img.onmouseout = function(){
+				if ($(this.id.substr(2)).style.display == "none")
+					this.src="chrome://backword/skin/backWordED.gif";
+			}
+			img.onclick = function(){
+				var r = $(this.id.substr(2));
+				r.style.display = (r.style.display == "")?"none":"";
+			}
+		}
+		else if(img.id.indexOf('delete') ==  0){
+			img.onclick = function(){
+				if (this.id.substr(7, 4) == "word"){
+					api.deleteWord(this.id.substr(12));
+				}
+				else{
+					var id = this.id.substr(13);
+					var i = id.indexOf('-');
+					api.deleteQuote(id.substring(0, i), id.substring(i+1, id.length));
+				}
+			}
+		}
+	}
+}
+
+function $(id){
+	return document.getElementById(id);
+}
+
 
